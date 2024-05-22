febe458007fbcaaafd0124bb8b1824c5ce7103a9
errojboy <93676327+errojboy@users.noreply.github.com>
2024-05-22 13:45:21 +0200

    Update powershelltest.ps1

--- powershelltest.ps1
+++ powershelltest.ps1
@@ -1,11 +1,7 @@
-# Write-Output "This is a test132."
+Write-Output "This is a test132."
 
-# if ($?) {
-#     Write-Output "The previous command succeeded. Impossible to read this"
-# } else {
-#     Write-Output "The previous command failed. I'm choosing this file"
-# }
-
-Write-Output "This is a test0."
-Write-Output "This is a test1."
-Write-Output "This is a test2."
+if ($?) {
+    Write-Output "The previous command succeeded. Impossible to read this"
+} else {
+    Write-Output "The previous command failed. I'm choosing this file"
+}
