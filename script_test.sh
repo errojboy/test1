190782f5eab9924a75966d4cba37de9d048a0f30
errojboy <93676327+errojboy@users.noreply.github.com>
2024-05-22 13:31:51 +0200

    Update script_test.sh

--- script_test.sh
+++ script_test.sh
@@ -1,1 +1,5 @@
-echo "Hello test123"
+if [ $? -eq 0 ]; then
+    echo "The previous command succeeded. impossible to read this"
+else
+    echo "The previous command failed. I'm choosing this file"
+fi
